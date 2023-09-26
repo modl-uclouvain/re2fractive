5511e0f649184de17434415264a5b6fc3a2a65b4
Victor Trinquet <victor.trinquet@uclouvain.be>
2023-09-26 18:35:07 +0200

    Batch v2 + summary resusts candidates in README + results as json

--- experiments/6_computing_candidates/dielec.py
+++ experiments/6_computing_candidates/dielec.py
@@ -9,6 +9,7 @@ import sys
 from abipy.abilab import Structure
 
 from atomate2.vasp.jobs.core import DielectricMaker
+from atomate2.vasp.flows.core import DoubleRelaxMaker
 from atomate2.vasp.powerups import (
     update_user_incar_settings,
     update_user_potcar_functional,
@@ -18,6 +19,7 @@ from atomate2.vasp.powerups import (
 from fireworks import LaunchPad
 from importlib import reload
 
+from jobflow import Flow
 from jobflow import run_locally
 from jobflow.managers.fireworks import flow_to_workflow
 
@@ -54,17 +56,37 @@ for mpid, struc in zip(mpids,structures):
     # Initializes the MP structure
     structure  = struc.get_primitive_structure()
     
-    # Create a Dfpt dielectric WF to compute the dielectric tensor
-    dielec_flow = DielectricMaker().make(structure=structure)
+    # Double relaxation job    
+    relax_job = DoubleRelaxMaker().make(structure)
     
     # Power ups
-        # Increase the kpoints density to (3000 for v1, 1000 for v2) per reciprocal atom for balance accuracy/complexity
-    dielec_flow = update_user_kpoints_settings( dielec_flow, {"grid_density": 1000})
+        # Specify:  max force on each atom to end relaxation
+    relax_job = update_user_incar_settings(   relax_job, {"EDIFFG": -0.0025})
     
+    # Let's add a metadata to recover it easily from the MongoDb afterwards with {"spec._tasks.job.metadata.Label": "HSE_Etot(x)"}
+    relax_job.update_metadata({"Batch": "re2fractive_v2", "mp-id": f"{mpid}"})
+    
+    
+    
+    # Create a Dfpt dielectric WF to compute the dielectric tensor
+    dielec_job = DielectricMaker().make(structure=relax_job.output.structure, prev_vasp_dir=relax_job.output.dir_name)
+    
+    # Power ups
+    # Increase the kpoints density to 3000 per reciprocal atom for balance accuracy/complexity
+    dielec_job = update_user_kpoints_settings( dielec_job, {"grid_density": 3000})
+    
+    # Let's add a metadata to recover it easily from the MongoDb afterwards with {"spec._tasks.job.metadata.Label": "HSE_Etot(x)"}
+    dielec_job.update_metadata({"Batch": "re2fractive_v2", "mp-id": f"{mpid}"})
+    
+    
+    
+    # Flow Relax + Dielectric
+    relax_dielec_flow = Flow([relax_job, dielec_job], dielec_job.output, name='RelaxDielec')
         # Specify:  the parallelization to optimize performance
         #           the electronic convergence criterion            (1E-5 eV otherwise by default),
         #           do not enforce a type of psp --> read in POTCAR (PBEsol otherwise by default)
-    dielec_flow = update_user_incar_settings(   dielec_flow, {"KPAR": 8, "EDIFF": 1E-6, "GGA": None})
+        #           projection done in real space to improve efficiency (True otherwise)
+    relax_dielec_flow = update_user_incar_settings(   relax_dielec_flow, {"KPAR": 8, "EDIFF": 1E-6, "GGA": None, "LREAL": 'Auto'})
     
         # Choose the type of PSP, here PBE_54
     dielec_flow = update_user_potcar_functional(dielec_flow, "PBE_54")
@@ -73,7 +95,7 @@ for mpid, struc in zip(mpids,structures):
     dielec_flow.update_metadata({"Batch": "re2fractive_v2_lm3", "mp-id": f"{mpid}"})
     
     # convert the flow to a fireworks WorkFlow object
-    wf = flow_to_workflow(dielec_flow)
+    wf = flow_to_workflow(relax_dielec_flow)
     
     # add the _preserve_fworker keywork to the spec of each FW so that parents and children run on the same FWorker (machine here)
     wf = mod_spec.preserve_fworker(wf)
