9d2e704796377e17044dbf4a2d9cfe34135c6884
Matthew Evans <git@ml-evs.science>
2023-10-27 11:42:35 +0200

    Some more refactoring and adding first tests

--- src/re2fractive/run.py
+++ src/re2fractive/run.py
@@ -14,7 +14,8 @@ MODNet will be used as the framework for prediction.
 from typing import Iterable
 from pathlib import Path
 import pandas as pd
-from re2fractive.campaign import Campaign, Dataset, OptimadeStructure
+from re2fractive.campaign import Campaign
+from re2fractive.datasets import NaccaratoDataset, MP2023Dataset
 
 from atomate2.vasp.jobs.core import DielectricMaker
 from modnet.models import EnsembleMODNetModel
@@ -23,45 +24,6 @@ from pymatgen.core import Structure
 
 class ModifiedDielectricMaker(DielectricMaker):
     pass
-
-
-class MP2023Dataset(Dataset):
-
-    properties = {"hull_distance": "_mp_energy_above_hull", "band_gap": "_mp_band_gap"}
-
-    def __init__(self):
-        import json
-
-        with open(
-            Path(__file__).parent.parent.parent
-            / "experiments/data/mp2023_structures.json"
-        ) as f:
-            structures = json.load(f)
-
-        self.data = [OptimadeStructure(s) for s in structures]
-
-
-class NaccaratoDataset(Dataset):
-
-    properties = {
-        "refractive_index": "_naccarato_refractive_index",
-        "band_gap": "_naccarato_gga_bandgap",
-        "hull_distance": "_mp_energy_above_hull",
-    }
-
-    def __init__(self):
-        import json
-
-        with open(
-            Path(__file__).parent.parent.parent / "experiments/data/structures.json"
-        ) as f:
-            structures = json.load(f)
-
-        self.data = [OptimadeStructure(structures[s]).entry.dict() for s in structures]
-
-        # fill in values for hull distance; if its in the MP then its low enough for us for now
-        for ind, d in enumerate(self.data):
-            self.data[ind]["attributes"]["_mp_energy_above_hull"] = 0
 
 
 def explore_stable_and_unique(
@@ -112,16 +74,20 @@ if __name__ == "__main__":
         oracles=[(["refractive_index"], lookup_refractive_index_oracle)],
         datasets=[NaccaratoDataset, MP2023Dataset],
         model=EnsembleMODNetModel,
+        initial_model=EnsembleMODNetModel.load(
+            str(
+                Path(__file__).parent.parent.parent
+                / "experiments/models/refractive_modnet.pkl"
+            )
+        ),
         logistics={
             "local": False,
             "jfr_project": "re2fractive",
             "jfr_preferred_worker": "lumi",
         },
         learning_strategy={"min_data_points": 100, "min_increment": 5},
-        explore_acquisition_function=explore_stable_and_unique,
-        acquisition_function=explore_stable_and_unique,
         drop_initial_cols=["refractive_index"],
     )
 
-    re2fractive.run()
-    re2fractive.dump_checkpoint("checkpoint.pkl")
+    re2fractive.run_setup()
+    re2fractive.run_epoch()
