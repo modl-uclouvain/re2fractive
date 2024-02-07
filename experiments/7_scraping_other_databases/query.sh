7eb033b23a4520635a4b0ec5fbcf30a06d21afee
Matthew Evans <git@ml-evs.science>
2024-02-07 15:00:43 +0100

    Add Alexandria structures and query script

---
+++ experiments/7_scraping_other_databases/query.sh
@@ -0,0 +1,1 @@
+optimade-get --max-results-per-provider -1 --filter '_alexandria_band_gap >= 0.3 AND _alexandria_hull_distance <= 0.025' https://alexandria.icams.rub.de/pbesol >> alexandria_pbesol_structures.json
