454aa1ddd7fdb19e869e5c602f8e3ae01cfb3d5d
Matthew Evans <git@ml-evs.science>
2024-04-26 13:38:35 +0200

    Add design space plot

--- src/re2fractive/plotting.py
+++ src/re2fractive/plotting.py
@@ -5,6 +5,98 @@ from modnet.preprocessing import MODData
 from plotly.subplots import make_subplots
 
 
+def plot_design_space(
+    campaign,
+    aux: str = "band_gap",
+):
+    colours = px.colors.qualitative.Plotly
+
+    point_colour = colours[0]
+    error_colour = colours[1]
+    point_colour_2 = colours[2]
+    point_colour_3 = colours[3]
+
+    fig = go.Figure()
+
+    fig.update_layout(
+        template="none",
+        font_family="Arial",
+        width=1000,
+        height=800,
+        title=f"Design space after {len(campaign.epochs)} iterations",
+    )
+
+    # plot initial oracle set
+    initial_dataset = campaign.datasets[0]
+    if isinstance(initial_dataset, type):
+        initial_dataset = initial_dataset.load()
+
+    target = next(iter(initial_dataset.targets))
+
+    target_name = initial_dataset.properties[target]
+    aux_name = initial_dataset.properties[aux]
+
+    target_y = [d.as_dict["attributes"][target_name] for d in initial_dataset.data]
+    target_x = [d.as_dict["attributes"][aux_name] for d in initial_dataset.data]
+
+    fig.add_trace(
+        go.Scatter(
+            x=target_x,
+            y=target_y,
+            mode="markers",
+            marker=dict(
+                size=5,
+                symbol="circle",
+                opacity=0.5,
+                line=dict(width=1, color="DarkSlateGrey"),
+                color=point_colour,
+            ),
+            name=initial_dataset.__class__.__name__,
+        ),
+    )
+
+    epoch = campaign.epochs[-1]
+    if len(campaign.datasets) > 1:
+        for ind, d in enumerate(campaign.datasets[1:]):
+            if isinstance(d, type):
+                d = d.load()
+            aux_name = d.properties[aux]
+            design_x = [entry.as_dict["attributes"][aux_name] for entry in d.data]
+            pred_y = epoch["design_space"]["predictions"][ind]
+            std_y = epoch["design_space"]["std_devs"][ind]
+
+            fig.add_trace(
+                go.Scatter(
+                    x=design_x,
+                    y=pred_y,
+                    error_y=dict(
+                        type="data",
+                        array=std_y,
+                        visible=True,
+                        color=error_colour,
+                    ),
+                    mode="markers",
+                    marker=dict(
+                        size=5,
+                        symbol="circle",
+                        opacity=1,
+                        line=dict(width=1, color="DarkSlateGrey"),
+                        color=point_colour_3,
+                    ),
+                    name=f"Predicted {d.__class__.__name__}",
+                ),
+            )
+
+    # Label axes
+    fig.update_xaxes(title_text=f"{aux_name}")
+    fig.update_yaxes(title_text=f"{target_name}")
+
+    # Start both axes at 0
+    fig.update_xaxes(range=[0, None], zeroline=False)
+    fig.update_yaxes(range=[0, None], zeroline=False)
+    fig.show()
+
+
 def parity_plot(
     test_moddata: MODData,
     predictions: pd.DataFrame,
